510d0e16d13a4fc454e178d20dcff712758ad2c3
RatanakT <ratanakthan.tr@gmail.com>
2022-07-07 16:02:49 +0700

    kom torn alert jkj

--- add_to_database.php
+++ add_to_database.php
@@ -9,28 +9,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
 <?php 
-   include 'config.php' ;
+   include 'fluent_db.php' ;
    if(isset($_POST['register'])){
     $name   = $_POST['name'];
     $amount      = $_POST['amount'];
     $price     = $_POST['price'];
    
-    $sql = "INSERT INTO product(name,amount,price) VALUES('$name','$amount','$price')";
-    $result = mysqli_query($connection,$sql);
+    $values = array('name' => $name, 'amount' => $amount,'price' => $price);
+    $query = $fluent->insertInto('product')->values($values)->execute();
    }
 ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript">
-            $(function(){
-                alert('You have successfully register!! Click "OK" for an other registration');
-            });
-</script>
 
-<?php
-include('config.php');
-$query = "SELECT id, name, amount, price FROM product";
-$result = $connection->query($query);
-?>
 <body class="d-flex justify-content-center">
     <div class="mt-4 m-4 p-5 w-50 " style="background-color: #D3EBCD; border-radius: 25px;">
     <h4>Registered Product</h4>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.20/sweetalert2.min.css">
     <title>Document</title>
 </head>
 <body style="margin: 100px;">
@@ -53,23 +54,21 @@
             </tr>";
 
             }
-			// while($row = $result->fetch()) {
-            //     echo "<tr>
-            //         <td>" . $row["id"] . "</td>
-            //         <td>" . $row["name"] . "</td>
-            //         <td>" . $row["amount"] . "</td>
-            //         <td>" . $row["price"] . "</td>
-            //         <td>
-            //             <a class='btn btn-primary btn-sm' href='update'>Update</a>
-            //             <a class='btn btn-danger btn-sm' href='delete.php?rn=$row[id]'>Delete</a>
-            //         </td>
-            //     </tr>";
-            // }
+		
             $fluent->close();
             
             ?>
         </tbody>
     </table>
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.20/sweetalert2.all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+
+
+
+
 </body>
 
 </html>
