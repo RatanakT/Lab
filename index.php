c96d25cc3c2e9a4f9d86422e69333c2492ab8a64
RatanakT <ratanakthan.tr@gmail.com>
2022-07-11 12:55:30 +0700

    latest updates nop

--- index.php
+++ index.php
@@ -12,10 +12,14 @@
     <link rel="stylesheet" href="sweetalert2.min.css">
 </head>
 <body style="margin: 100px;">
-   <div class="d-flex justify-content-between">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.20/sweetalert2.all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+   
+    <div class="d-flex justify-content-between">
      <h3 >List of Products</h3>
-     <a class='btn btn-success btn-sm' href='add.php' > <i class="fa-solid fa-plus"></i>  Add more product</a>
-
+     <a class='btn btn-success btn-sm'  class="btn btn-primary" data-toggle="modal" data-target="#editmodal" > <i class="fa-solid fa-plus"></i>  Add more product</a>
+     
    </div>
     <br>
     <table class="table table-bordered">
@@ -49,8 +53,8 @@
                 <td>" . $row["amount"] . "</td>
                 <td>" . $row["price"] . "</td>
                 <td>
-                    <a class='btn btn-primary btn-sm' href='update'>Update</a>
-                    <a class='btn btn-danger btn-sm' href='delete.php?rn=$row[id]'>Delete</a>
+                    <a class='btn btn-primary btn-sm' href='update.php'>Update</a>
+                    <button id='btndelete' name='delete' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#deletemodal' >Delete</button>
                 </td>
             </tr>";
 
@@ -61,16 +65,132 @@
             ?>
         </tbody>
     </table>
-   
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.20/sweetalert2.all.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
 
 
+    <!-- POP UP TO ADD PRODUCT -->
+    <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Add Product </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form  action="add.php" id="form" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="update_id" id="update_id">
+
+                        <div class="form-group">
+                            <label>Name </label>
+                            <input type="text" name="name" id="name" class="form-control"
+                                placeholder="Enter Product Name">
+                        </div>
+                        <br>
+
+                        <div class="form-group">
+                            <label> Amount </label>
+                            <input type="text" name="amount" id="amount" class="form-control"
+                                placeholder="Enter Product Amount ">
+                        </div>
+                        <br>
+
+                        <div class="form-group">
+                            <label> Price </label>
+                            <input type="text" name="price" id="price" class="form-control"
+                                placeholder="Enter Product Price">
+                        </div>
+                        <br>
+
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button id="confirmadd"  type="submit" name="submit" class="btn btn-primary">Confirm</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+     <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
+     <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form action="delete.php" method="POST">
+                    <div class="modal-body">
+
+                        <input type="hidden" name="delete_id" id="delete_id">
+
+                        <h4> Do you want to Delete this Data ??</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button id="delete" type="submit" name="deletedata" class="btn btn-primary"> Yes !! Delete it. </button>
+                        <a type="button" href="index.php" class="btn btn-secondary"> NO </a>
+                        
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
 
 
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script>
+      $(function () {
 
+        $('#confirmadd').on('click', function () {
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Product Added Successfully',
+                showConfirmButton: false,
+                timer: 1500
+                })
+        });
+
+      });
+    </script>
+       <script>
+        $(document).ready(function () {
+
+            $('.btn-danger').on('click', function () {
+
+                $('#deletemodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#delete_id').val(data[0]);
+
+            });
+        });
+    </script>
+    <script>
+      $(function () {
+        $('#delete').on('click', function () {
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Product Delete Successfully',
+                showConfirmButton: false,
+                timer: 1500
+                })
+        });
+
+      });
+    </script>  
 </body>
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 </html>
